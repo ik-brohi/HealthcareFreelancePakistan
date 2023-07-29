0d462ed45dc384bd07f3674f9ccae65d6dda964e
aftabahmedabro <aftab.bscsf19@iba-suk.edu.pk>
2023-07-29 18:38:15 +0500

    Errors resolved of backend

--- lib/models/doctor_provider.dart
+++ lib/models/doctor_provider.dart
@@ -1,32 +1,34 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecom/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ecom/models/doctor_model.dart';
 
-
-
 class DoctorProvider extends ChangeNotifier {
   CollectionReference doctorsCollection =
-  FirebaseFirestore.instance.collection('doctors');
+      FirebaseFirestore.instance.collection('doctor');
   List<Doctor> doctors = [];
+ 
+
 
   Future<void> fetchDoctors() async {
-    try{
-      final snapshot = await doctorsCollection.get();
+    try {
+  
+      QuerySnapshot querySnapshot = await doctorsCollection.get();
 
-      doctors = snapshot.docs.map((doc) {
-        final data = doc.data();
+      doctors = querySnapshot.docs.map((doc) {
+        // final data = doc.data();
         return Doctor(
-          name: data["name"],
-          title: data["title"],
-          location: data["location"],
-          hourlyRate: data["hourlyRate"].toDouble(),
-          ratings: data["ratings"].toDouble(),
-          description: data["description"].toString(),
+          id: doc.id,
+          name: doc.get("name"),
+          title: doc.get("title"),
+          location: doc.get("location"),
+          hourlyRate: doc.get("hour"),
+          ratings: doc.get("ratings"),
+          description: doc.get("description"),
         );
       }).toList();
-    }
-    catch(e){
+    } catch (e) {
       print(e);
     }
 
@@ -35,6 +37,7 @@ class DoctorProvider extends ChangeNotifier {
 
   Future<void> addDoctor(Doctor doctor) async {
     await doctorsCollection.add({
+      'ID'  : doctor.id,
       'name': doctor.name,
       'title': doctor.title,
       'location': doctor.location,
@@ -47,9 +50,10 @@ class DoctorProvider extends ChangeNotifier {
   }
 
   Future<void> updateDoctor(Doctor doctor) async {
-    final doctorDoc = doctorsCollection.doc(doctor.id);
+    final doctorDoc = doctorsCollection.doc();
 
     await doctorDoc.update({
+      'id': doctor.id,
       'name': doctor.name,
       'title': doctor.title,
       'location': doctor.location,
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../models/user.dart';
 import 'authenticate/authenticate.dart';
 import 'home/home.dart';
 
