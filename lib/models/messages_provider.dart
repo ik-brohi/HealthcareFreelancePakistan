88b064b092c510d8256f56494af3c2dc352daf96
Aftab Ahmed <73740818+AftabAhmedAbro@users.noreply.github.com>
2023-08-12 00:46:04 +0500

    Create messages_provider.dart

---
+++ lib/models/messages_provider.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class MessageProvider extends ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  List<Message> _messages = [];
+
+  List<Message> get messages => _messages;
+
+  Future<void> fetchMessages() async {
+    final QuerySnapshot snapshot =
+        await FirebaseFirestore.instance.collection('messages').get();
+
+    _messages = snapshot.docs.map((doc) {
+      final data = doc.data();
+      return Message(
+        id: doc.id,
+        text: data['text'],
+        userId: data['userId'],
+        timestamp: data['timestamp'].toDate(),
+      );
+    }).toList();
+
+    notifyListeners();
+  }
+
+  Future<void> sendMessage(String text) async {
+    final currentUser = _auth.currentUser;
+    if (currentUser != null) {
+      await FirebaseFirestore.instance.collection('messages').add({
+        'text': text,
+        'userId': currentUser.uid,
+        'timestamp': FieldValue.serverTimestamp(),
+      });
+
+      notifyListeners();
+    }
+  }
+}
