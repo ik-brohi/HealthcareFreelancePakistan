733b1183a0f9bb1831b2a89dacac5efd3015fa6f
Aftab Ahmed <73740818+AftabAhmedAbro@users.noreply.github.com>
2023-08-12 00:48:24 +0500

    Create messages.dart

---
+++ lib/models/messages.dart
@@ -0,0 +1,13 @@
+class Message {
+  final String id;
+  final String text;
+  final String userId;
+  final DateTime timestamp;
+
+  Message({
+    required this.id,
+    required this.text,
+    required this.userId,
+    required this.timestamp,
+  });
+}
